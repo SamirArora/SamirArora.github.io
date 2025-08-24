410bd32f6f0c1dc688f859a9a9d5890f27c5dd61
SamirArora <94946741+SamirArora@users.noreply.github.com>
2025-08-24 18:37:11 +0000

    Deploying to gh-pages from @ SamirArora/SamirArora.github.io@ec6d47bc2ee562d2629504aef560ec1dda250f55 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -454,35 +454,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%73%61%6D%69%72_%61%72%6F%72%61@%73%66%75.%63%61", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/SamirArora", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/samirarora001", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-researchgate',
+        title: 'ResearchGate',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.researchgate.net/profile/Samir_Arora/", "_blank");
         },
       },{
       id: 'light-theme',
