d7575291d964758e9da211aadc5ce72361f7cd66
SamirArora <94946741+SamirArora@users.noreply.github.com>
2025-08-24 17:13:00 +0000

    Deploying to gh-pages from @ SamirArora/SamirArora.github.io@fa308e883fbda16c314ec65fefe1e19ebc1ca21b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,16 +398,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-delivered-a-talk-at-international-conference-on-statistics-and-data-science-2025",
+          title: 'Delivered a talk at International Conference on Statistics and Data Science 2025.”',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
